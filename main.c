5392422d6a235849bc26f50c8eec03091eb4463f
Julien Castiaux <Julien Castiaux>
2017-02-12 20:55:01 +0100

    Use 7447 instead of 7448, C code to test the 7-seg display

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <main.h>
 
+int toBCD(int binary) {
+   if (binary >= 0 && binary < 100)
+      return ((binary / 10) << 4) + binary % 10;
+   else if (binary < 0)
+      return 0;
+   else
+      return 99;
+}
+
 void main() {
    output_c(0b11);
+   output_d(toBCD(15));
 }
