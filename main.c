f36ef2b9121376f0087e11ab8dfe69d485f58185
Julien00859 <Julien.castiaux@gmail.com>
2017-02-17 18:56:48 +0100

    add max232 (serial converter), lm35 (temperature probe), 471 (amplifier), use one decoder (4511 instead of 7447) for two 7-seg (common cathode instead of anode), fix the red led to be off when green in on, update C code for the 7-seg, C code to retrieve the temperature

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
-#include <main.h>
+#include <18F458.h>
+#device ADC=10
+#use delay(crystal=20000000)
 
-int toBCD(int binary) {
-   if (binary >= 0 && binary < 100)
-      return ((binary / 10) << 4) + binary % 10;
-   else if (binary < 0)
-      return 0;
-   else
-      return 99;
-}
+#define BCD_UNITS(x) (x % 10)
+#define BCD_TENS(x) (x / 10 % 10)
 
-void main() {
-   output_c(0b11);
-   output_d(toBCD(15));
+void main()
+{
+   setup_adc_ports(AN0);
+   setup_adc(ADC_CLOCK_INTERNAL);
+   setup_low_volt_detect(FALSE);
+
+   int seuil = 35;
+   int temperature;
+
+   while(TRUE)
+   {
+      temperature = read_adc() * 100 / 1024;
+      if (temperature >= seuil)
+         output_c(0b10);
+      else
+         output_c(0b01);
+      
+      output_d(BCD_UNITS(temperature) | (1 << 4));
+      delay_ms(10);
+      output_d(BCD_TENS(temperature) | (1 << 5));
+      delay_ms(10);
+   }
 }
--- main.h
+++
@@ -1,4 +0,0 @@
-#include <18F458.h>
-#device ADC=10
-#use delay(crystal=20000000)
-
