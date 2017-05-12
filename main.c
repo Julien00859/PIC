0ceadcdb06459e0c3e2033ea4fcdbc33da1a0a45
bastiaens <olivier.bastiaens@gmail.com>
2017-05-12 16:43:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 #include <18F458.h>
 #device ADC=10
 #use delay(crystal=20000000)
-#use rs232(baud=57600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8)
+#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
 
-#define BCD_UNITS(x) (x % 10)
-#define BCD_TENS(x) (x / 10)
 void gestionLed(int temp, int seuil);
 void affTemp(int temperature);
 void main()
@@ -12,18 +10,25 @@ void main()
    setup_adc_ports(AN0);
    setup_adc(ADC_CLOCK_INTERNAL);
    setup_low_volt_detect(FALSE);
-
+   
+   
    int seuil = 35;
-   int temperature;
+   int temperature = 10;
 
    while(TRUE)
-   {
+   { 
       temperature = read_adc() * 100 / 1024;
       gestionLed(temperature,seuil); 
       affTemp(temperature);
-      printf("%d",temperature);
-      seuil = getc();
-      
+    
+      if(temperature > seuil){
+         printf("%c", temperature | (1 >> 7));
+      }
+      else{
+         printf("%c", temperature | (0 >> 7));
+      }
+
+      //delay_ms(500);
    }
 }
 
@@ -40,9 +45,9 @@ void gestionLed(int temp, int seuil){
 
 }
 void affTemp(int temperature){
-      output_d(BCD_UNITS(temperature) | (1 << 4));
+      output_d((temperature % 10) | (1 << 4));
       delay_ms(10);
-      output_d(BCD_TENS(temperature) | (1 << 5));
+      output_d((temperature / 10) | (1 << 5));
       delay_ms(10);
 }
 
